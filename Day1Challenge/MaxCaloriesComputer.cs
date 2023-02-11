78cc3e5f5173f19038096d3a9ccd9acbd2c646c9
Molrat <j.somers301@gmail.com>
2023-02-11 18:14:22 +0100

    initial commit

---
+++ Day1Challenge/MaxCaloriesComputer.cs
@@ -0,0 +1,26 @@
+﻿namespace Day1Challenge
+{
+    internal class MaxCaloriesComputer
+    {
+        internal static int ComputeMaxCalories(string list)
+        {
+            List<string> lines = list.Split('\n').ToList();
+            int totalCalories = 0;
+            int maxCalories = 0;
+            foreach(string line in lines)
+            {
+                if (line.Length > 0) 
+                { 
+                    totalCalories += Convert.ToInt32(line);
+                }
+                else
+                {
+                    maxCalories = Math.Max(maxCalories, totalCalories);
+                    totalCalories = 0;
+                }
+            }
+            maxCalories = Math.Max(maxCalories, totalCalories);
+            return maxCalories;
+        }
+    }
+}
---
+++ Day1Challenge/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day1Challenge;
+
+Console.WriteLine("Hello, World!");
+string input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+int result = MaxCaloriesComputer.ComputeMaxCalories(input);
+Console.WriteLine(result);
